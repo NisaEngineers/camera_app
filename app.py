fa6be04bebfd9dd8309bef46605da32958271447
NisaEngineers <71565114+NisaEngineers@users.noreply.github.com>
2023-09-05 23:11:57 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+st.title('AzimHealth AI Fitness Trainer')
+
+
+recorded_file = 'output_sample.mp4'
+sample_vid = st.empty()
+sample_vid.video(recorded_file)
+
+    
+    
+
+    
+
+
